c238a6a55b89890391943c0805d0b145a554ec18
SitiGayatriHehanussa <61864166+SitiGayatriHehanussa@users.noreply.github.com>
2020-04-14 16:12:05 +0700

    Add files via upload

---
+++ DemoSatu.java
@@ -0,0 +1,9 @@
+class DemoSatu {
+public static void main (String[] args){
+System.out.println("awal program");
+int x = 10;
+x = x / 0;
+System.out.println(x);
+ System.out.println("akhir program");
+}
+}
